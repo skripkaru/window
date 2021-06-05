99487c7a3de5bba5025bded36745ee3700572f00
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2021-06-05 09:35:50 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+// const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, './src/js/main.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, './src/index.html'),
+            inject: 'body',
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: './src/assets', to: './'},
+            ],
+        }),
+        // new SVGSpritemapPlugin('./src/svg/**/*.svg', {
+        //     output: {
+        //         filename: 'sprite.svg',
+        //     },
+        //     sprite: {
+        //         prefix: false,
+        //     }
+        // })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    }
+}
